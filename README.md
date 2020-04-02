767dc6b746c227a915db00eda60ae1f9844b4f36
Stephen Daly <stephencdaly@gmail.com>
2020-04-02 16:06:18 +0100

    Use individual env vars for database creds

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Playing around with creating an open banking demo in Go
 
 ```docker run -d -p 5432:5432 --name openbanking -e POSTGRES_PASSWORD=mysupersecretpassword postgres```
 
-```DATABASE_URL=postgres://postgres:mysupersecretpassword@localhost:5432/?sslmode=disable ./stephens-openbanking-test```
+```DB_NAME=postgres DB_HOSTNAME=localhost:5432 DB_USERNAME=postgres DB_PASSWORD=mysupersecretpassword ./stephens-openbanking-test```
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/stephencdaly/stephens-openbanking-test/http"
 	"github.com/stephencdaly/stephens-openbanking-test/database"
+	"github.com/stephencdaly/stephens-openbanking-test/http"
 )
 
 func Main() error {
-	db, err := database.NewDB(os.Getenv("DATABASE_URL"))
+	var userName = os.Getenv("DB_USERNAME")
+	var password = os.Getenv("DB_PASSWORD")
+	var hostname = os.Getenv("DB_HOSTNAME")
+	var dbName = os.Getenv("DB_NAME")
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", userName, password, hostname, dbName)
+	db, err := database.NewDB(connStr)
 	if err != nil {
 		return err
 	}
